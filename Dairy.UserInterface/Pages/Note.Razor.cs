0ceeab7221e480ca62b1aab83d6f1b1e76d13735
sh-vasily <vasia.punk.rok@gmail.com>
2023-01-29 15:42:19 +0300

    fix note editing

--- Dairy.UserInterface/Pages/Note.Razor.cs
+++ Dairy.UserInterface/Pages/Note.Razor.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using System.Text;
 using Dairy.ViewModels;
 using Microsoft.AspNetCore.Components;
@@ -9,12 +10,24 @@ public partial class Note
 {
     [Parameter]
     public long? NoteId { get; set; }
+    
+    public string Text { get; set; }
 
     [Inject] 
     private HttpClient HttpClient { get; set; }
 
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        var noteViewModel = await HttpClient.GetFromJsonAsync<NoteViewModel>(
+            $"http://localhost:5555/Note/{NoteId}");
+
+        Text = noteViewModel.Text;
+    }
+
     private async Task OnChange(string noteContent, string textarea)
     {
+        Text = noteContent;
         var noteViewModel = new NoteViewModel(NoteId.Value, DateTime.UtcNow, noteContent);
         var payload = JsonConvert.SerializeObject(noteViewModel);
         var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
