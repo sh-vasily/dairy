d31fad672484cdbf93c8a0fc484dd46ea3a49344
Василий Щербаков <basil-scherbakov@ya.ru>
2023-11-10 09:44:00 +0300

    refactor

--- Dairy.UserInterface/Program.cs
+++ Dairy.UserInterface/Program.cs
@@ -8,7 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddScoped<INoteService, NoteService>();
 
 await builder.Build().RunAsync();
